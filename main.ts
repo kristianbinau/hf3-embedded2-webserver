4d99493e6c8ba9e841310df1aec29cc79658d956
Kristian Binau <kristian@binau.me>
2021-09-21 10:52:19 +0200

    Init

---
+++ main.ts
@@ -0,0 +1,29 @@
+import {
+    Application,
+    HttpRequest,
+    HttpResponse,
+    RequestMethod,
+} from "https://deno.land/x/dragon@v1.1.6/lib/mod.ts";
+
+const app = new Application();
+
+const r = app.routes();
+
+r.withPath("/hello")
+    .withMethods(RequestMethod.GET)
+    .handleFunc(
+        async function (Request: HttpRequest, ResponseWriter: HttpResponse) {
+            ResponseWriter.end("Hello Dragon");
+        },
+    );
+
+r.withPath("/demo")
+    .handleFunc(
+        async function (Request: HttpRequest, ResponseWriter: HttpResponse) {
+            ResponseWriter.end("Hello Dragon Demo");
+        },
+    );
+
+app.listenAndServe({ port: 8084 });
+
+console.log("🐉 Serveur listining");
