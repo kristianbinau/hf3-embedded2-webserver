801bfc127ae390153b1029e211ec870d1817f4be
Kristian Binau <kristian@binau.me>
2021-09-22 09:07:13 +0200

    Add temp endpoint

--- main.ts
+++ main.ts
@@ -4,6 +4,14 @@ import {
     HttpResponse,
     RequestMethod,
 } from "https://deno.land/x/dragon@v1.1.6/lib/mod.ts";
+import { Client } from "https://deno.land/x/mysql/mod.ts";
+
+const client = await new Client().connect({
+    hostname: "127.0.0.1",
+    username: "root",
+    password: "root",
+    db: "hf3-embedded-webserver-test",
+});
 
 const app = new Application();
 
@@ -17,10 +25,16 @@ r.withPath("/hello")
         },
     );
 
-r.withPath("/demo")
+r.withPath(/temp\/(?<temp>[0-9]+\.?[0-9]*|\.[0-9]+)/u).withMethods(RequestMethod.GET)
     .handleFunc(
         async function (Request: HttpRequest, ResponseWriter: HttpResponse) {
-            ResponseWriter.end("Hello Dragon Demo");
+            const { temp: temperature } = Request.params();
+            const temp = temperature as unknown as number
+
+            let result = await client.execute(`INSERT INTO data(data_name, value) values(?,?)`, [
+                "temp",
+                1000 * temp,
+            ]);
         },
     );
 
